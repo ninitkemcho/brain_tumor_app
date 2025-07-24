6b9eb9c79e7342ea8cff5f2b5d11703edd31152b
ninitkemcho <123997303+ninitkemcho@users.noreply.github.com>
2025-07-24 18:22:47 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,30 +1,18 @@
 import torch
-from torchvision import transforms
-from PIL import Image
-import os
-import requests
-
-def preprocess_image(image):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.Grayscale(num_output_channels=1),
-        transforms.ToTensor(),
-        transforms.Normalize([0.5], [0.5])
-    ])
-    return transform(image).unsqueeze(0)
-
-def download_model_from_url(url, filename):
-    if not os.path.exists(filename):
-        print("Downloading model...")
-        r = requests.get(url)
-        with open(filename, 'wb') as f:
-            f.write(r.content)
-        print("Download complete.")
 
 def load_model(model_path):
-    from model import BrainTumorResNet
-    model = BrainTumorResNet(num_classes=4)
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'), weights_only=False))
-
+    model = torch.load(model_path, map_location=torch.device('cpu'))
     model.eval()
     return model
+
+def preprocess_image(image):
+    from torchvision import transforms
+    from PIL import Image
+    transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+    if isinstance(image, Image.Image):
+        return transform(image).unsqueeze(0)
+    return transform(Image.open(image)).unsqueeze(0)
