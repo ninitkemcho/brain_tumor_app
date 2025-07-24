fb5a7290db654a139f5ba33260afb4a64b843062
ninitkemcho <123997303+ninitkemcho@users.noreply.github.com>
2025-07-24 18:40:19 +0400

    Update model.py

--- model.py
+++ model.py
@@ -5,4 +5,5 @@ def get_model():
     model = models.resnet18(pretrained=False)
     model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)  # for grayscale
     model.fc = nn.Linear(model.fc.in_features, 4)  # 4 output classes
+    
     return model
