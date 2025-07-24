e389f4de414048140405e99c7c67544743ccbe34
ninitkemcho <123997303+ninitkemcho@users.noreply.github.com>
2025-07-24 19:31:02 +0400

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,14 @@ import torch.nn as nn
 import torchvision.models as models
 
 def get_model():
-    model = models.resnet18(pretrained=False)
-    model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)  # for grayscale
-    model.fc = nn.Linear(model.fc.in_features, 4)  # 4 output classes
-    return model
+    class BrainTumorResNet(nn.Module):
+        def __init__(self, num_classes=4):
+            super(BrainTumorResNet, self).__init__()
+            self.resnet = models.resnet18(pretrained=True)
+            self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
+
+        def forward(self, x):
+            return self.resnet(x)
+
+    return BrainTumorResNet()
